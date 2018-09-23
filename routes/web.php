f16a17999570d58a5282e9f26775ba41e3f59901
Samuel Visscher <samuel@visscher.io>
2018-09-23 20:28:28 +0400

    Implement basis for user.campaigns

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::get('/home', 'HomeController@index', [
 ])->name('home');
 
 Route::middleware(['auth'])->group(function () {
+
     Route::middleware(['admin'])->namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
 
         Route::resource('campaigns', 'CampaignController');

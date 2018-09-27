df79a56c602e9021d123d991a0a0d1c7e9151e5a
Samuel Visscher <samuel@visscher.io>
2018-09-27 22:17:16 +0400

    Use dummyimage

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(\App\Models\Image::class, function (Faker $faker) {
 
     return [
-        'url' => $faker->imageUrl(640, 480, 'cats'),
+        'url' => 'https://dummyimage.com/640x480/ddd/888',
         'name' => 'Featured Image'
     ];
 });

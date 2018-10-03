4239f6cec944168ced47ed50e6ea03c601ec7a0e
Giga Gatenashvili <g.gatenashvili@gmail.com>
2018-10-03 16:08:58 +0400

    - Changed Image::fromFile() so that is supports image resize and thumbnail creation.

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -8,40 +8,16 @@ use Illuminate\Http\FileHelpers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
-use ImageProcessor;
-
 class ImageController extends Controller
 {
     public function upload(Request $request) {
         $image_descriptors = [];
         foreach ($request->featured_images as $featured_image) {
 
-            //TODO: Quality of images are getting very low after fit() call.
-            //TODO: Define best size here
-            $processedImageData = (string) ImageProcessor::make($featured_image)->fit(640, 480)->stream();
-            $thumbnailImageData = (string) ImageProcessor::make($featured_image)->fit(80, 59)->stream();
-
-            //Relatives
-            $processedImagePath = $featured_image->hashName();
-            $thumbnailImagePath = 'thumbnails/'. $featured_image->hashName();
-
-            //TODO: Move it in Image Model class and create method like fromStream or something like that.
-            // Or maybe add third options parameter to from file and pass array of processing values
-            //FIXME: Should we check for success here?
-            Storage::disk('s3')->put($processedImagePath, $processedImageData, ['visibility' => 'public']);
-            Storage::disk('s3')->put($thumbnailImagePath, $thumbnailImageData, ['visibility' => 'public']);
-
-            //dd(Storage::disk('s3')->url($processedImagePath), Storage::disk('s3')->url($thumbnailImagePath));
-
-            $image = new Image();
-            $image->name = 'Featured image';
-            $image->url = Storage::disk('s3')->url($processedImagePath);
-            $image->path = $processedImagePath;
-            $image->thumbnail_url = Storage::disk('s3')->url($thumbnailImagePath);
-            $image->thumbnail_path = $thumbnailImagePath;
-            $image->save();
-
-            //$image = Image::fromFile($featured_image, 'Featured Image');
+            $image = Image::fromFile($featured_image, 'Featured Image', [
+                'fit' => [640, 480],
+                'thumbnailFit' => [80, 59]
+            ]);
 
             $descriptor = [
                 'name' => $featured_image->getClientOriginalName(),
@@ -52,8 +28,6 @@ class ImageController extends Controller
                 'deleteType' => 'DELETE',
                 'id' => $image->id,
             ];
-
-            //dd($descriptor);
 
             $image_descriptors[] = $descriptor;
         }

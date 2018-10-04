7566396eb204ac7373f8ea1ad4d907be90354308
Giga Gatenashvili <g.gatenashvili@gmail.com>
2018-10-04 10:33:57 +0400

    - Made featured_image_id nullable for campaign.
    - Admin\CampaignController cleanup.

--- app/Http/Controllers/Admin/CampaignController.php
+++ app/Http/Controllers/Admin/CampaignController.php
@@ -33,37 +33,7 @@ class CampaignController extends Controller
         $campaign->name = $request->input('name');
         $campaign->details = $request->input('details');
         $campaign->author_id = Auth::user()->id;
-        //$campaign->video_url = $request->video;
-        //$campaign->ethereum_address = $request->ethereum_address;
-
-        /*$featured_images = $request->featured_images;
-        $featured_image_entities = [];
-
-        if($featured_images != null) {
-            foreach ($featured_images as $featured_image) {
-                $image = Image::fromFile($featured_image, 'Featured Image');
-                Storage::disk('s3')->url($image->url);
-
-                $featured_image_entities[] = $image;
-            }
-        }*/
-
-        //$image = Image::fromFile($request->file('featured_image'), 'Featured Image');
-
-        //TODO: Temporary associating empty image, need to decide if create form should have featured image and what
-        // kind of input should we use for it jQuery ajax or just input[type=file]
-
-        $image = new Image();
-        $image->name = 'Empty image';
-        $image->url = 'http://sssa.com';
-        $image->save();
-
-        $campaign->featured_image()->associate($image);
         $campaign->save();
-
-        /*foreach ($featured_image_entities as $featured_image_entity) {
-            $campaign->images()->save($featured_image_entity);
-        }*/
 
         return redirect(route('admin.campaigns.edit', $campaign->id));
     }
@@ -84,63 +54,21 @@ class CampaignController extends Controller
 
     public function update(UpdateCampaign $request, $id)
     {
-        //FIXME: TMP
-        //FIXME: This parameter can be easily changed by user on front-end.
-        // User can assign whichever images he wants to this campaign.
-        $imageIds = $request->image_ids;
-
-        //FIXME: END TMP
-
         $campaign =  Campaign::findOrFail($id);
         $campaign->name = $request->input('name');
         $campaign->details = $request->input('details');
         $campaign->video_url = $request->video;
         $campaign->ethereum_address = $request->ethereum_address;
         $campaign->status_id = CampaignStatus::idFromName($request->status);
-        //$campaign->author_id = Auth::user()->id;
-
-        $featured_images = $request->featured_images;
-        $featured_image_entities = [];
-
-        if($featured_images != null) {
-            foreach ($featured_images as $featured_image) {
-                $image = Image::fromFile($featured_image, 'Featured Image');
-                Storage::disk('s3')->url($image->url);
-
-                $featured_image_entities[] = $image;
-            }
-        }
 
 
-        //FIXME: TMP disable
-        /*$featured_image = $request->featured_image;
-
+        $featured_image = $request->featured_image;
         if($featured_image !== null) {
             $image = Image::fromFile($featured_image, 'Featured Image');
 
             $campaign->featured_image()->delete();
             $campaign->featured_image()->associate($image);
-        }*/
-
-        //FIXME: TMP
-        if($imageIds !== null && is_array($imageIds)) {
-
-            foreach ($imageIds as $imageId) {
-                $image = Image::find($imageId);
-                $image->campaign_id = $campaign->id;
-                $image->save();
-            }
-
-            //dd('we are here', $campaign->images);
         }
-        //FIXME: END TMP
-
-        //TODO: We replace old featured pictures should we append?
-        //FIXME: TMP disable
-        /*$campaign->images()->delete();
-        foreach ($featured_image_entities as $featured_image_entity) {
-            $campaign->images()->save($featured_image_entity);
-        }*/
 
         $campaign->save();
 

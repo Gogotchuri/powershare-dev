639120c48bc7374fd3b42ea1c9af5f09ddcb8201
Giga Gatenashvili <g.gatenashvili@gmail.com>
2018-10-10 18:01:54 +0400

    - Added terms of use to native registration.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -55,9 +55,12 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
+            'agree' => 'required',
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+        ], [
+            'agree.required' => 'You should agree to Terms of use'
         ]);
     }
 
--- resources/views/auth/shared/terms.blade.php
+++ resources/views/auth/shared/terms.blade.php
@@ -2,22 +2,43 @@
     <label class="col-xs-3 control-label">Terms of use</label>
     <div class="col-xs-9">
         <div style="border: 1px solid #e5e5e5; height: 200px; overflow: auto; padding: 10px;">
-            <p>Lorem ipsum dolor sit amet, veniam numquam has te. No suas nonumes recusabo mea, est ut graeci definitiones. His ne melius vituperata scriptorem, cum paulo copiosae conclusionemque at. Facer inermis ius in, ad brute nominati referrentur vis. Dicat erant sit ex. Phaedrum imperdiet scribentur vix no, ad latine similique forensibus vel.</p>
-            <p>Dolore populo vivendum vis eu, mei quaestio liberavisse ex. Electram necessitatibus ut vel, quo at probatus oportere, molestie conclusionemque pri cu. Brute augue tincidunt vim id, ne munere fierent rationibus mei. Ut pro volutpat praesent qualisque, an iisque scripta intellegebat eam.</p>
-            <p>Mea ea nonumy labores lobortis, duo quaestio antiopam inimicus et. Ea natum solet iisque quo, prodesset mnesarchum ne vim. Sonet detraxit temporibus no has. Omnium blandit in vim, mea at omnium oblique.</p>
-            <p>Eum ea quidam oportere imperdiet, facer oportere vituperatoribus eu vix, mea ei iisque legendos hendrerit. Blandit comprehensam eu his, ad eros veniam ridens eum. Id odio lobortis elaboraret pro. Vix te fabulas partiendo.</p>
-            <p>Natum oportere et qui, vis graeco tincidunt instructior an, autem elitr noster per et. Mea eu mundi qualisque. Quo nemore nusquam vituperata et, mea ut abhorreant deseruisse, cu nostrud postulant dissentias qui. Postea tincidunt vel eu.</p>
-            <p>Ad eos alia inermis nominavi, eum nibh docendi definitionem no. Ius eu stet mucius nonumes, no mea facilis philosophia necessitatibus. Te eam vidit iisque legendos, vero meliore deserunt ius ea. An qui inimicus inciderint.</p>
+            <p>Lorem ipsum dolor sit amet, veniam numquam has te. No suas nonumes recusabo mea, est ut graeci
+                definitiones. His ne melius vituperata scriptorem, cum paulo copiosae conclusionemque at. Facer inermis
+                ius in, ad brute nominati referrentur vis. Dicat erant sit ex. Phaedrum imperdiet scribentur vix no, ad
+                latine similique forensibus vel.</p>
+            <p>Dolore populo vivendum vis eu, mei quaestio liberavisse ex. Electram necessitatibus ut vel, quo at
+                probatus oportere, molestie conclusionemque pri cu. Brute augue tincidunt vim id, ne munere fierent
+                rationibus mei. Ut pro volutpat praesent qualisque, an iisque scripta intellegebat eam.</p>
+            <p>Mea ea nonumy labores lobortis, duo quaestio antiopam inimicus et. Ea natum solet iisque quo, prodesset
+                mnesarchum ne vim. Sonet detraxit temporibus no has. Omnium blandit in vim, mea at omnium oblique.</p>
+            <p>Eum ea quidam oportere imperdiet, facer oportere vituperatoribus eu vix, mea ei iisque legendos
+                hendrerit. Blandit comprehensam eu his, ad eros veniam ridens eum. Id odio lobortis elaboraret pro. Vix
+                te fabulas partiendo.</p>
+            <p>Natum oportere et qui, vis graeco tincidunt instructior an, autem elitr noster per et. Mea eu mundi
+                qualisque. Quo nemore nusquam vituperata et, mea ut abhorreant deseruisse, cu nostrud postulant
+                dissentias qui. Postea tincidunt vel eu.</p>
+            <p>Ad eos alia inermis nominavi, eum nibh docendi definitionem no. Ius eu stet mucius nonumes, no mea
+                facilis philosophia necessitatibus. Te eam vidit iisque legendos, vero meliore deserunt ius ea. An qui
+                inimicus inciderint.</p>
         </div>
     </div>
 </div>
 
 <div class="form-group">
     <div class="col-xs-6 col-xs-offset-3">
-        <div class="checkbox">
-            <label>
-                <input type="checkbox" name="agree" value="agree" /> Agree with the terms and conditions
-            </label>
+        <div class="form-group">
+            <div class="form-check">
+                <input name="agree" class="form-check-input @if($errors->has('agree')) is-invalid @endif" type="checkbox" value="agree"
+                       id="agree">
+                <label class="form-check-label" for="agree">
+                    Agree to terms and conditions
+                </label>
+                @if ($errors->has('agree'))
+                    <div class="invalid-feedback">
+                        You should agree before submitting.
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
 </div>

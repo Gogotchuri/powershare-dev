3faad284216a63b47c048e031be68d8541c3b326
Giga Gatenashvili <g.gatenashvili@gmail.com>
2018-10-15 12:59:12 +0400

    - Removed unused methods from TeamMemberControllrs

--- app/Http/Controllers/Admin/TeamMemberController.php
+++ app/Http/Controllers/Admin/TeamMemberController.php
@@ -12,16 +12,6 @@ use Intervention\Image\Facades\Image as IntImage;
 class TeamMemberController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
@@ -54,17 +44,6 @@ class TeamMemberController extends Controller
         $member->save();
 
         return back()->with('message', 'New team member "'. $member->name .'" added');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\TeamMember  $teamMember
-     * @return \Illuminate\Http\Response
-     */
-    public function show(TeamMember $teamMember)
-    {
-        //
     }
 
     /**

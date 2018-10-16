46317f46b2195601425c65d1ce74091806bc15af
Giga Gatenashvili <g.gatenashvili@gmail.com>
2018-10-16 18:11:05 +0400

    - Fixed video overflow on about page.

--- resources/views/public/about.blade.php
+++ resources/views/public/about.blade.php
@@ -34,7 +34,7 @@
                                 * For more details, visit <a href="http://fire.powershare.fund">fire.powershare.fund</a>.
                             </p>
 
-                            <div class="col-md-12 text-center">
+                            <div class="videoWrapper">
                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/k5vwG_BofJ0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                             </div>
                         </div>

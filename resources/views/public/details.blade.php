72e259c3ee1d237bdbff41bcb472cf7fee46b1c2
Giga Gatenashvili <g.gatenashvili@gmail.com>
2018-10-18 14:54:22 +0400

    - Fixed connect-menu z-index, added spacing to gallery items on campaign page.

--- resources/views/public/details.blade.php
+++ resources/views/public/details.blade.php
@@ -155,14 +155,14 @@
                                                     @break
                                                 @endif
                                                 <div class="col-xl-4">
-                                                    <div class="gallery-item">
+                                                    <div class="gallery-item mb-4">
                                                         <div class="gallery-image" style="background-image: url({{$image->url}})"></div>
                                                     </div>
                                                 </div>
                                             @endforeach
                                             @if($campaign->youtube_id)
                                                         <div class="col-xl-4">
-                                                            <div class="gallery-item">
+                                                            <div class="gallery-item mb-4">
                                                                 <div class="videoWrapper mb-5">
                                                                     <iframe width="560" height="315" src="https://www.youtube.com/embed/{{$campaign->youtube_id}}?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                                                                 </div>

84696cdb04cfbfb52720741028328ac757698fe5
Giga Gatenashvili <g.gatenashvili@gmail.com>
2018-10-19 12:53:42 +0400

    - Using same shadow mixins accros website.

--- resources/views/public/details.blade.php
+++ resources/views/public/details.blade.php
@@ -164,7 +164,7 @@
                                                                 <div class="video-container">
                                                                     <div class="video-bg cover">
                                                                         <div class="video-fg">
-                                                                            <iframe width="560" height="315" src="https://www.youtube.com/embed/{{$campaign->youtube_id}}?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                                                            <iframe width="560" height="315" src="https://www.youtube.com/embed/{{$campaign->youtube_id}}?showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                                                                         </div>
                                                                     </div>
                                                                 </div>

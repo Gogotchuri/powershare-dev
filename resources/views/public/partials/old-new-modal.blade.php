7e64dd498250ef28209e65ba1b8b578a20debe39
Giga Gatenashvili <g.gatenashvili@gmail.com>
2018-10-23 18:49:38 +0400

    - Old/new website popup almost finished with styling.

--- resources/views/public/partials/old-new-modal.blade.php
+++ resources/views/public/partials/old-new-modal.blade.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div id="oldNewModal" class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-dialog-centered " role="document">
         <div class="modal-content">
             {{--<div class="modal-header">--}}
                 {{--<h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>--}}
@@ -10,21 +10,41 @@
             {{--</div>--}}
             <div class="modal-body">
                 <div class="container-fluid">
-                    <h1 class="text-center">Welcome to powershare</h1>
+                    <h1 class="text-center">Welcome to POWERSHARE</h1>
                     <div class="row mb-3 text-center">
                         <div class="col-md-6">
-                            <h6>Visit Token Sale Page</h6>
+                            <div class="campaign-mark">
+                                <div class="campaign-mark-img">
+                                    <img src="/img/icons/token.png">
+                                </div>
+                                <div class="campaign-mark-title">
+                                    <span>Visit Token Sale Page</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="campaign-mark">
+                                <div class="campaign-mark-img">
+                                    <img src="/img/icons/token.png">
+                                </div>
+                                <div class="campaign-mark-title">
+                                    <span>Proceed to the Alpha Platform</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3 text-center">
+                        <div class="col-md-6">
                             <button type="button" class="btn btn-primary old-new-choice-button" data-target-url="http://fire.powershare.fund" >fire.powershare.fund</button>
                         </div>
                         <div class="col-md-6">
-                            <h6>Proceed to the Alpha Platform</h6>
                             <button type="button" class="btn btn-primary old-new-choice-button" data-target-url="https://powershare.fund">powershare.fund</button>
                         </div>
                     </div>
                     <div class="row text-center">
                         <div class="col-md-12">
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input chooseWebsiteTick" type="checkbox" value="option1">
+                                <input class="form-check-input check_box chooseWebsiteTick" type="checkbox" value="option1">
                                 <label class="form-check-label" for="inlineCheckbox1">Do not show this message again.</label>
                             </div>
                         </div>

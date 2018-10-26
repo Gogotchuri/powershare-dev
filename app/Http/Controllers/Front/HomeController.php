70fbc19109882a13c8e9f7405b1b1c87a981838c
Giga Gatenashvili <g.gatenashvili@gmail.com>
2018-10-26 19:28:08 +0400

    - Removed test code.

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use App\Models\Campaign;
 use App\Models\Reference\CampaignStatus;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -14,13 +13,9 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function index()
     {
         $campaigns = Campaign::where('status_id', CampaignStatus::APPROVED)->paginate(8);
-
-        if($request->ajax()) {
-            sleep(5);
-        }
 
         return view('public.home', compact('campaigns'));
     }

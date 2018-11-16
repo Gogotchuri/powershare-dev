2176585d43ce44fbac769d5107feb5d8f3f9096b
Giga Gatenashvili <g.gatenashvili@gmail.com>
2018-11-16 18:15:55 +0400

    - Fixed show campaign management page.

--- resources/views/user/campaigns/show.blade.php
+++ resources/views/user/campaigns/show.blade.php
@@ -9,17 +9,13 @@
         @elseif($campaign->is_proposal && Auth::user()->ownsCampaign($campaign->id))
             <div class="alert alert-warning mt-3" role="alert">
                 <p>Submited for review.</p>
-                <form method="post" action="{{route('user.campaigns.delete', ['id' => $campaign->id])}}">
+                <form method="post" action="{{route('user.campaigns.destroy', ['id' => $campaign->id])}}">
                     @method("DELETE")
                     @csrf
                     <div class="btn-group">
                         <button type="submit" class="btn btn-danger">Delete Campaign</button>
                     </div>
                 </form>
-            </div>
-        @else
-            <div class="alert alert-success mt-3" role="alert">
-                <a href="{{route('public.campaign.show', ['id' => $campaign->id])}}" target="_blank">Public</a>
             </div>
         @endif
     @endsection

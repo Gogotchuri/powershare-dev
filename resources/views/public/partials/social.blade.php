b96f7ac6a865122447ec9b32444a3bd31c7aa616
Giga Gatenashvili <g.gatenashvili@gmail.com>
2018-12-11 18:54:46 +0400

    - Added medium social icon.

--- resources/views/public/partials/social.blade.php
+++ resources/views/public/partials/social.blade.php
@@ -2,3 +2,4 @@
 <li><a target="_blank" href="https://twitter.com/pwrshr"><img src="/img/icons/twitter.png"></a></li>
 <li><a target="_blank" href="https://t.me/joinchat/Hao74QxTfNa_tFLGJvFjpQ"><img src="/img/icons/telegram.png"></a></li>
 <li><a target="_blank" href="https://facebook.com/POWERSHARE.FUND"><img src="/img/icons/facebook.png"></a></li>
+<li><a target="_blank" href="https://medium.com/powershare"><img src="/img/icons/medium.png"></a></li>
